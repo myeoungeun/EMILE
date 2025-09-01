1a083af6b8b4ad20a6054473fe672f439afb08ed
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-09-01 11:11:47 +0900

    [feat] 사용할 스크립트 생성

---
+++ Assets/02.Scripts/Monster/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    // 몬스터 스탯 데이터 SO
+    /*
+     * 몬스터 데이터가 가져야 하는 것
+     * 체력, 공격력, 공격 속도, 이름, 사용 프리팹?, 총알 프리팹 리스트, 이동속도, 고유 ID, 방어력, 공격 방식
+     */
+    protected int curHp;
+
+    
+
+}
