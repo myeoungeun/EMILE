f99dac4b7222b9211b4c5c041cca6735164f8e04
Heoyoon <parechae@gmail.com>
2025-09-01 14:22:35 +0900

    feat : 비동기 씬 로드 기능추가

---
+++ Assets/02.Scripts/UI/Scene/SceneButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneButton : MonoBehaviour
+{
+    [SerializeField]string sceneName;
+    public void OnButtonClick()
+    {
+        AsyncSceneManager.GetInstance.AsyncSceneLoad(sceneName);
+    }
+}
