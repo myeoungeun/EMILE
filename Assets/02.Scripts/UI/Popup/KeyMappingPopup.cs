eb62740d7a76d40e69717bfc4c946e505288bf0b
SeungYeop Oh <sadrain9274@naver.com>
2025-09-03 02:52:55 +0900

    [feat] 팝업 스크립트에 PopupBase 상속

--- Assets/02.Scripts/UI/Popup/KeyMappingPopup.cs
+++ Assets/02.Scripts/UI/Popup/KeyMappingPopup.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class OptionPopup : PopupBase
+public class KeyMappingPopup : PopupBase
 {
+    public override void Initialize()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
