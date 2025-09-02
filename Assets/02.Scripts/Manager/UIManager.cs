75a96efc030f9b8c488c6942df7e0655f3ef7273
SeungYeop Oh <sadrain9274@naver.com>
2025-09-03 02:54:02 +0900

    [feat] UIManager UI,Popup 열기,닫기 기능 추가

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -1,38 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
 
+public enum UIType
+{
+    Title,
+    InGame,
+    GameOver,
+}
+
+public enum PopupType
+{
+    Option,
+    KeyMapping,
+}
+
 public class UIManager : MonoSingleton<UIManager>
 {
+    [Header("UI")]
     [SerializeField] private TitleUI titleUI;
     [SerializeField] private InGameUI inGameUI;
     [SerializeField] private GameOverUI gameOverUI;
 
-    public TitleUI TitleUI => titleUI;
-    public InGameUI InGameUI => inGameUI;
-    public GameOverUI GameOverUI => gameOverUI;
+    [Header("Popup")]
+    [SerializeField] private OptionPopup optionPopup;
+    [SerializeField] private KeyMappingPopup keyMappingPopup;
 
-    public void Initialize() // TODO 초기화해야할 함수 추가
+    private Dictionary<UIType, UIBase> uiDictionary; // 일반UI 관리
+    private Dictionary<PopupType, PopupBase> popupDictionary; // 팝업UI 관리
+
+    protected override void Awake()
     {
-        titleUI.gameObject.SetActive(true);
-        inGameUI.gameObject.SetActive(false);
-        gameOverUI.gameObject.SetActive(false);
+        base.Awake();
+        UIManagerInitialize();
     }
 
-    public void ShowTitleUI()
+    #region 초기화
+    private void UIManagerInitialize()
     {
+        //일반 UI 딕셔너리 초기화
+        uiDictionary = new Dictionary<UIType, UIBase>
+        {
+            { UIType.Title, titleUI },
+            { UIType.InGame, inGameUI },
+            { UIType.GameOver, gameOverUI }
+        };
 
+        //팝업 UI 딕셔너리 초기화
+        popupDictionary = new Dictionary<PopupType, PopupBase>
+        {
+            {PopupType.Option, optionPopup},
+            {PopupType.KeyMapping, keyMappingPopup}
+        };
+
+        //모든 일반 UI 초기화
+        foreach (var ui in uiDictionary.Values)
+        {
+            ui.Initialize();
+        }
+
+        //모든 팝업 UI 초기화
+        foreach (var popup in popupDictionary.Values)
+        {
+            popup.Initialize();
+        }
+    }
+    #endregion
+    
+    public void ShowUI(UIType type)
+    {
+        foreach (var ui in uiDictionary.Values)
+        {
+            ui.Close(); // 모든 일반UI 닫기
+        }
+        uiDictionary[type].Open(); // 요청한 일반UI 열기
     }
 
-    public void ShowInGameUI()
+    public void ShowPopup(PopupType type)
     {
-
+        popupDictionary[type].Open(); // 요청한 팝업UI 열기
     }
 
-    public void ShowGameOverUI()
+    public void ClosePopup(PopupType type)
     {
-
+        popupDictionary[type].Close(); // 요청한 팝업UI 닫기
     }
 }
