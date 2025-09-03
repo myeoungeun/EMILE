52231ec9110adb362b1068f20f80151cae0a1147
SeungYeop Oh <sadrain9274@naver.com>
2025-09-03 18:18:23 +0900

    [feat] GameOverUi 스크립트 추가 및 타이틀 돌아가기 버튼 이벤트 추가

--- Assets/02.Scripts/UIs/UI/GameOverUI.cs
+++ Assets/02.Scripts/UIs/UI/GameOverUI.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 public class GameOverUI : UIBase
 {
+    [SerializeField] private Button exitButton;
     public override void Initialize()
     {
+        exitButton.onClick.AddListener(OnExitButton);
+    }
 
+    public void OnExitButton()
+    {
+        UIManager.Instance.ShowUI(UIType.Title); // Title 전환
     }
 }
