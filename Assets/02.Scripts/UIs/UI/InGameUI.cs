2804848a5ad6d83e7f26bd0cba8816e226e31fc7
SeungYeop Oh <sadrain9274@naver.com>
2025-09-04 11:51:44 +0900

    [feat] InGameUI 초기화 기능 추가

--- Assets/02.Scripts/UIs/UI/InGameUI.cs
+++ Assets/02.Scripts/UIs/UI/InGameUI.cs
@@ -4,11 +4,19 @@ using UnityEngine.UI;
 
 public class InGameUI : UIBase
 {
+    [Header("HUD")]
     [SerializeField] private PlayerHUD playerHUD;
     [SerializeField] private BossHUD bossHUD;
 
+    public PlayerHUD PlayerHUD => playerHUD;
+    public BossHUD BossHUD => bossHUD;
+
     public override void Initialize()
     {
+        playerHUD.Initialize();
+        bossHUD.Initialize();
 
+        playerHUD.Open(); // 항상 켜짐
+        bossHUD.Close(); // 보스전 전까지 비활성
     }
 }
