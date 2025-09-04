e389f4bf3f17d6f3fb66d0ade14ab7c7a96d85f8
SeungYeop Oh <sadrain9274@naver.com>
2025-09-04 11:49:34 +0900

    [chore] BulletSlotUI 변수명 수정

--- Assets/02.Scripts/UIs/UI/BulletSlotUI.cs
+++ Assets/02.Scripts/UIs/UI/BulletSlotUI.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 
 public class BulletSlotUI : MonoBehaviour
 {
-    [SerializeField] private Image bulletImage; // 탄약 이미지
-    [SerializeField] private TextMeshProUGUI bulletCountText; // 탄약 개수
+    [SerializeField] private Image bulletIcon; // 탄약 이미지
+    [SerializeField] private TextMeshProUGUI bulletCount; // 탄약 개수
     [SerializeField] private GameObject highlightBorder; // 선택 탄약 강조
     [SerializeField] private Image cooldown; // 탄약 쿨다운(재장전)
 
-    private int currentBulletCount; // 현재 탄약 개수
+    private int curBulletCount; // 현재 탄약 개수
     // 슬롯 초기화
     public void Initialize()
     {
-        currentBulletCount = 0;
-        bulletCountText.text = "0";
+        curBulletCount = 0;
+        bulletCount.text = "0";
 
         SetSelected(false); // 기본 선택 해제
     }
@@ -35,15 +35,15 @@ public class BulletSlotUI : MonoBehaviour
     // 탄약 개수 갱신
     public void SetBulletCount(int count)
     {
-        currentBulletCount = count;
-        bulletCountText.text = currentBulletCount.ToString();
+        curBulletCount = count;
+        bulletCount.text = curBulletCount.ToString();
     }
 
     // 탄약 소모시 1씩 감소 - UI갱신
     public void UseBullet()
     {
-        if (currentBulletCount < 0) return;
-        currentBulletCount--;
-        SetBulletCount(currentBulletCount);
+        if (curBulletCount < 0) return;
+        curBulletCount--;
+        SetBulletCount(curBulletCount);
     }
 }
