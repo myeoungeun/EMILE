04df99c41d55b874601f2f6542f1e277c9e717a8
SeungYeop Oh <sadrain9274@naver.com>
2025-09-06 14:36:53 +0900

    [feat] 데드존 스크립트 추가

---
+++ Assets/DeadZone.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Stat.TakeDamage(player.Stat.CurHP);
+            }
+        }
+    }
+}
